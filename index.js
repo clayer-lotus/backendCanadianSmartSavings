b3483f12d1e9e59b1bad2cdb6271897a0f05631f
dev-lotus <lotusbiswas2022@gmail.com>
2022-11-04 14:52:55 +0530

    all files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 
 const https = require('https')
-const request=require('request')
+const request = require('request')
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -15,43 +15,55 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use((req,res,next1) => {
+app.use((req, res, next1) => {
     console.log("Use Header")
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next1();
-    });
-app.get("/", (req,res)=>{
+});
+app.get("/", (req, res) => {
     res.send("Hello");
 });
 
-app.post('/postDetails', function (req, res) {
+app.post('/canadian-smart-savings/get-a-quote', function (req, res) {
+    console.log(req.body);
 
+    request({
+        uri: "https://workflow-automation.podio.com/catch/2328e470au0u194",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body)
 
-	request({
-	uri:"https://workflow-automation.podio.com/catch/annh165148d03c2",
-	method:"POST",
-	headers:{"Content-Type":"application/json"},
-	body:JSON.stringify(req.body)
+    }, function (err, resp, body) {
 
-    // body: JSON.stringify({
-    //     "firstName": "Marcos",
-    //     "lastName": "Silva",
-    //     "email": "marcos.henrique@toptal.com",
-    //     "password": "s3cr3tp4sswo4rd"
-    // })
+        if (resp.statusCode != 200) res.sendStatus(400)
+        else res.json({ "status": "success" });
 
-},function(err,resp,body){
-	
-     if(resp.statusCode!=200) res.sendStatus(400)
-     	else res.json({"status":"success"});
-
-});
+    });
 
 
 })
 
-app.listen(port, ()=>{
+app.post('/canadian-smart-savings/schedule-service', function (req, res) {
+    console.log(req.body);
+
+    request({
+        uri: "https://workflow-automation.podio.com/catch/6td647n6038c800",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body)
+
+    }, function (err, resp, body) {
+
+        if (resp.statusCode != 200) res.sendStatus(400)
+        else res.json({ "status": "success" });
+
+    });
+
+
+})
+
+app.listen(port, () => {
     console.log("I am live again");
 })
 
