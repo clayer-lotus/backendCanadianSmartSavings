fae7bd9e24a87929d36dd7a16c875dde457771c3
dev-lotus <lotusbiswas2022@gmail.com>
2022-11-26 07:22:09 +0530

    new account api

--- index.js
+++ index.js
@@ -63,6 +63,28 @@ app.post('/canadian-smart-savings/schedule-service', function (req, res) {
 
 })
 
+// NEW ACCOUNT
+
+app.post('/canadian-smart-savings/new-account', function (req, res) {
+    console.log(req.body);
+
+    request({
+        uri: "https://workflow-automation.podio.com/catch/c53i0xb87z8rdt5",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body)
+
+    }, function (err, resp, body) {
+
+        if (resp.statusCode != 200) res.sendStatus(400)
+        else res.json({ "status": "success" });
+
+    });
+
+
+})
+
+
 // LAUREL WEBSITE TRY A FREE SESSION
 
 app.post('/trifit-wellness/try-a-free-session', function (req, res) {
